995250a3b3d13ccef7f8d3517c5a456a909dce8e
BigMonkeyyy <1085511430@qq.com>
2020-06-01 19:50:13 +0800

    单调栈

--- 84  柱状图中最大的矩形/单调栈.js
+++ 84  柱状图中最大的矩形/单调栈.js
@@ -2,9 +2,21 @@
  * @param {number[]} heights
  * @return {number}
  */
- var largestRectangleArea = function(heights) {
-  
+var largestRectangleArea = function (heights) {
+    let res = 0;
+    let stack = [];   // 维护一个单调递增栈
+    heights = [0, ...heights, 0];
+    heights.forEach((curHeight, index) => {
+        while (stack.length > 0 && curHeight < heights[stack[stack.length - 1]]) {
+            // 当前bar比栈顶bar矮
+            let topIndex = stack.pop();    // 弹出栈顶，保留栈顶索引
+            res = Math.max(res, heights[topIndex] * (index - stack[stack.length - 1] - 1));
+        }
+        // 当前bar比栈顶高或者与栈顶高度相等
+        stack.push(index);
+    });
+    return res;
 };
 
-let arr = [4,2,0,3,2,5];
+let arr = [2,1,5,6,2,3];
 console.log(largestRectangleArea(arr));
